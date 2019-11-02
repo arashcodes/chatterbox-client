f42df0e75c3d5589771a34d8649ca2bedc4658da
arashcodes <s.arash.abbasi@gmail.com>
2019-11-01 17:14:54 -0700

    Displays messages on DOM

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,27 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    Parse.readAll((data) => {
+      // examine the response from the server request:
+      // console.log('Hello from MessagesView', data);
+
+      this.renderMessage(data.results);
+    });
+
   },
 
-  render: function() {
+  renderMessage: function(results) {
+    let html = '';
+    for (let i = 0; i < results.length; i++) {
+
+      if ((results[i].username && results[i].roomname) && results[i].text) {
+        console.log(results[i]);
+        html += MessageView.render(results[i]);
+      }
+
+
+    }
+    $('#chats').append(html);
   }
 
 };
